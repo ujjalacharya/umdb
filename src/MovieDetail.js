9e20735f56864e9911d5b139a985d513555dbf1f
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-03-28 21:18:38 +0545

    styled components

--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Poster } from './Movie'
+import styled from 'styled-components'
+import Overdrive from 'react-overdrive'
 
 const POSTER_PATH = 'https://image.tmdb.org/t/p/w154';
 const BACKDROP_PATH = 'https://image.tmdb.org/t/p/w1280';
 
 
 class MoviesList extends Component {
-    
+
 
     state = {
         movie: []
@@ -14,7 +17,7 @@ class MoviesList extends Component {
     async componentDidMount() {
 
         try {
-            const res = await fetch('https://api.themoviedb.org/3/movie/'+this.props.match.params.id+'?api_key=d4f67f412d4bb016866b72002ccb2158');
+            const res = await fetch('https://api.themoviedb.org/3/movie/' + this.props.match.params.id + '?api_key=d4f67f412d4bb016866b72002ccb2158');
 
             const movie = await res.json();
             this.setState({
@@ -26,16 +29,22 @@ class MoviesList extends Component {
     }
 
     render() {
-        const {movie} = this.state; 
-        
+        const { movie } = this.state;
+
         return (
             <div>
-                <img src={`${BACKDROP_PATH}${movie.backdrop_path}`} alt={movie.title} />
-                <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
-                
-               <h2>{movie.title}</h2> 
-               <h4>{movie.release_date}</h4>
-               <p>{movie.overview}</p>
+                <MovieWrapper backdrop={`${BACKDROP_PATH}${movie.backdrop_path}`}>
+                    <MovieInfo>
+                        <Overdrive id={movie.id}>
+                            <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+                        </Overdrive>
+                        <div>
+                            <h2>{movie.title}</h2>
+                            <h4>{movie.release_date}</h4>
+                            <p>{movie.overview}</p>
+                        </div>
+                    </MovieInfo>
+                </MovieWrapper>
             </div>
 
         );
@@ -43,3 +52,23 @@ class MoviesList extends Component {
 }
 
 export default MoviesList;
+
+const MovieWrapper = styled.div`
+position: relative;
+padding-top: 50vh;
+background: url(${props => props.backdrop}) no-repeat;
+background-size: cover
+`
+const MovieInfo = styled.div`
+background: white;
+text-align: left;
+padding: 2rem 10%;
+display: flex
+>div{
+    margin-left: 20px
+}
+img{
+    position: relative;
+    top: -5rem
+}
+`
--- src/MoviesList.js
+++ src/MoviesList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Movie from './Movie';
+import styled from 'styled-components'
+
 
 class MoviesList extends Component {
 
@@ -23,12 +25,20 @@ class MoviesList extends Component {
 
     render() {
         return (
-            <div>
+            <MovieGrid>
                 {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
-            </div>
+            </MovieGrid>
+
 
         );
     }
 }
 
 export default MoviesList;
+
+const MovieGrid = styled.div`
+    display: grid;
+    padding: 1rem;
+    grid-template-columns: repeat(6, 1fr);
+    grid-row-gap: 1rem;
+`
