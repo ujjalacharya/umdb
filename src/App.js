d991f21c996c8803dee3c2711e38bd518f25b923
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-03-28 13:42:42 +0545

    initializing the actual app

--- src/App.js
+++ src/App.js
@@ -3,27 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  
-  state={
-    input: ''
-  }
 
-  handleChange = (event)=>{
-    this.setState({
-      input: event.target.value
-    })
-  }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">Submit the form</p>
-        <input onChange={this.handleChange} type="text" value={this.state.input}/>
-        <h3>Hello Mr.{this.state.input} </h3>
       </div>
     );
   }
