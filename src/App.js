08bad6d16531598fb0c4a7d59d8f56ae30a345ef
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-03-28 12:55:11 +0545

    basic ref

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 
 class App extends Component {
 
- 
+  handleSubmit = ()=>{
+    console.log(this.text.value);
+  }
 
   render() {
     return (
@@ -13,7 +15,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-    
+        <p className="App-intro">Submit the form</p>
+        <input type="text" ref={(input)=>this.text = input}/>
+        <button onClick={this.handleSubmit}>Submit</button>
       </div>
     );
   }
